86e7d0e139b8f82ddf57e4da0786af49ea1103ae
Mikku0 <mikolaj080803@gmail.com>
2024-08-15 22:37:55 +0200

    added basic views | extracted footer to single file | added links to pages

--- base/views.py
+++ base/views.py
@@ -4,7 +4,25 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'base/index.html')
 
-def dept(request):
-    return render(request, 'base/thankyou.html')
+def about(request):
+    return render(request, 'base/about.html')
+
+def contact(request):
+    return render(request, 'base/contact.html')
+
+def shop_single(request):
+    return render(request, 'base/shop-single.html')
+
+def cart(request):
+    return render(request, 'base/cart.html')
+
+def checkout(request):
+    return render(request, 'base/checkout.html')
+
+def shop(request):
+    return render(request, 'base/shop.html')
+
+def thank_you(request):
+    return render(request, 'base/thank-you.html')
